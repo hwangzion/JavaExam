5d58ac007d51a217e57c30006dcb5e5d065fabcd
황지온 <s2654@e-mirim.hs.kr>
2026-07-27 18:28:36 +0900

    Fix class name and improve output formatting

--- MinExclude_1318.java
+++ MinExclude_1318.java
@@ -1,10 +1,11 @@
 package main.Test.test2026_1_2.A;
+
 import java.util.*;
 //1318황지온
-public class MInExclude_1318 {
+public class MinExclude_1318 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("점수 개수 : ");
+        System.out.print("점수 개수 : ");
         int num = sc.nextInt();
         double sum = 0;
         double[] arr = new double[num];
@@ -22,12 +23,12 @@ public class MInExclude_1318 {
                 min = arr[i];
             }
         }
+
         System.out.print("\n제거된 최솟값 : " + min);
         for(int i = 0; i < arr.length; i++){
-            if(min != arr[i]){
-                sum += arr[i];
-            }
+            sum += arr[i];
         }//최솟값 제외 합계
+        sum -= min;
         System.out.println("\n최솟값 제외 합계 : " + sum);
         System.out.printf("최솟값 제외 평균 : %.2f", sum / (num - 1));
     }
